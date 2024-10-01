dc716e1aa2dcc9f3fe4f28e0cf214d35438c1f6b
LeVietHa05 <ha.leviet.0504@gmail.com>
2024-10-01 16:26:29 +0700

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -12,24 +12,20 @@
 #define DIR1 3
 #define EN2 4
 #define DIR2 5
+#define FULL_ROUND_STEP 800
+int pos1 = 0;
+int pos2 = 0;
 //--------------------------------------
 // relay pins
 #define PUMPPIN 6
 #define VALVEPIN 7
 //--------------------------------------
-// potentiometer pins
-#define STEPPER1_CONTROL_PIN A0
-#define STEPPER2_CONTROL_PIN A1
-//--------------------------------------
-// button pins
-// TODO:  change the pin
-#define IGNITE_IN_PIN 14  // the button
-#define IGNITE_LED_PIN 15 // led of the button
-//--------------------------------------
 // nrf24 defines &
 #define CE 9
 #define CSN 10
 const uint64_t address = 0xf0f0f0f066;
+//--------------------------------------
+unsigned long lastTime = 0;
 //==================================================================================================
 AccelStepper stepper1(AccelStepper::DRIVER, EN1, DIR1);
 AccelStepper stepper2(AccelStepper::DRIVER, EN2, DIR2);
@@ -40,10 +36,10 @@ RF24 radio(CE, CSN);
 //==================================================================================================
 struct ControlData // 8 bytes 8 channels
 {
-  byte ch1;
-  byte ch2;
-  byte ch3;
-  byte ch4;
+  int ch1;  // 0-1023
+  int ch2;  // 0-1023
+  byte ch3; // 0-1
+  byte ch4; // 0-1
   byte ch5;
   byte ch6;
   byte ch7;
@@ -64,8 +60,6 @@ void setup()
   //--------------------------------------
   pinMode(PUMPPIN, OUTPUT);
   pinMode(VALVEPIN, OUTPUT);
-  pinMode(IGNITE_IN_PIN, INPUT_PULLUP);
-  pinMode(IGNITE_LED_PIN, OUTPUT);
   //--------------------------------------
   if (!radio.begin())
   {
@@ -81,19 +75,48 @@ void setup()
   {
     Serial.println(F("radio hardware is  responding!!"));
   }
-  radio.setAutoAck(true);
-  radio.setDataRate(RF24_1MBPS); // Tốc độ dữ liệu
-  radio.setChannel(100);
-  radio.setPALevel(RF24_PA_LOW);
+  radio.setAutoAck(false);
+  radio.setDataRate(RF24_250KBPS); // Tốc độ dữ liệu
+  // radio.setChannel(100);
+  // radio.setPALevel(RF24_PA_LOW);
   radio.openReadingPipe(1, address);
   radio.startListening();
+
+  controlData.ch1 = 1024 / 2;
+  controlData.ch2 = 1024 / 2;
+  controlData.ch3 = 0;
+  controlData.ch4 = 0;
+  controlData.ch5 = 0;
+  controlData.ch6 = 0;
+  controlData.ch7 = 0;
+  controlData.ch8 = 0;
+  //--------------------------------------
+}
+//==================================================================================================
+void revData()
+{
+  while (radio.available())
+  {
+    radio.read(&controlData, sizeof(controlData));
+    lastTime = millis();
+  }
 }
 //==================================================================================================
 void loop()
 {
-  if (radio.available())
+  revData();
+  pos1 = map(controlData.ch1, 0, 1023, 0, FULL_ROUND_STEP);
+  pos1 = map(controlData.ch2, 0, 1023, 0, FULL_ROUND_STEP);
+  steppers.moveTo(pos1, pos2);
+  steppers.setSpeed(1000);
+  steppers.runSpeedToPosition(); // non-blocking
+  if (controlData.ch3 == 1)
   {
-    radio.read(&controlData, sizeof(controlData));
+    dw(PUMPPIN, HIGH);
+  }
+  else
+  {
+    dw(PUMPPIN, LOW);
   }
 }
 //==================================================================================================
